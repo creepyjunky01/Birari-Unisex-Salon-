f430a9e3664b94250f0ce292255743e204abf65e
creepyjunky01 <creepyjunky01@gmail.com>
2026-09-22 11:31:46 +0530

    Remove old customer route after route-group move

--- birari-unisex-salon/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getSettings } from "@/lib/settings";
-import LandingFlow from "@/components/LandingFlow";
-
-export default async function HomePage() {
-  const settings = await getSettings();
-
-  return <LandingFlow settings={settings} />;
-}
